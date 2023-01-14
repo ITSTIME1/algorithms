bde5bf7081d6b3a56f1e551d396bdfc07701d988
itstime <itstime0809@gmail.com>
2023-01-14 18:03:34 +0900

    rotating data-structure

---
+++ data_structure/N번째 큰 수.py
@@ -0,0 +1,22 @@
+# 문제분석
+
+import sys
+
+
+n = int(sys.stdin.readline().strip())
+#  그러면 여기서...
+# 아냐 이렇게 하기 보다는
+# 음...
+# 뭔가 이렇게 하면 안될거 같은데..
+
+# list 사용하지 말라는거 같은데
+
+
+
+index = n
+ans = 0
+
+
+
+
+
