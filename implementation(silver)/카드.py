078d4c9de1e9ef73d52c9202a827999fcdb0272b
ITSTIME <itstime0809@gmail.com>
2023-01-26 16:34:56 +0900

    fail problem because i didn't think the solve

--- implementation(silver)/카드.py
+++ implementation(silver)/카드.py
@@ -1,1 +1,82 @@
 # 문제분석
+
+# 숫자카드를 n장 가지고 있다고 할 때
+
+# 그 숫자카드 n장에 각각 정수가 하나 적혀있다고 한다.
+
+# 준규가 가지고 있는 카드가 주어졌을때
+
+# 가장 많이 가지고 있는 정수를 구하라?
+
+# 가장 많이 가지고 있는 정수가 여러가지라면
+# 작은 것을 출력해라
+
+# 아 되게 간단한거같은데
+
+# 1
+# 2
+# 1
+# 2
+# 1
+# 1: 3
+# 2: 2
+
+
+# 1
+# 2
+# 1
+# 2
+# 1
+# 2
+# 1:3
+# 2:3
+
+# 가장 많이 가지고 있는 정수가 동일한 개수를 가지고 있지 않을경우
+# 그냥 많이 가지고 있는걸 출력하면 될거고
+
+# 가장 많이 가지고 있는 수가 동일하다면
+# 가장 작은걸 출력한다.
+
+# hash 를 이용했을때
+# max함수를 이용해서 key 를 얻는다면
+# maxvalue 가 여러개라도\
+# 맨 앞에 있는 key 값 하나만 출력하게 된다
+# 하지만 리스트커프리헨션을 사용하게 된다면
+# maxValue 에 해당하는 모든 키 값들을 다 가지고 올 수 있다
+# 딱 이문제에서 사용하기 좋은 것 같네
+
+# n^2 시간초과가 나니까
+
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+# 이부분 시간초과를 해결해야 하는데
+# in을 쓰면 시간초과인데
+# set 을 써보면 될거 같은데
+
+
+# set 에서 시간복잡도는 리스트에 비해서 int 메서드는 O(1) 의 시간복잡도를 요구한다
+# 아니면 리스트를 사용해서 만들어야 할까
+# TLE;;
+number_check, dic = [], {}
+for i in range(N):
+	num = int(input())
+	number_check.append(num)
+
+number_set = set(number_check)
+
+for i in number_set:
+	dic[i] = number_check.count(i)
+
+# python 에서 set = > hashtable로 구성되어져있다
+filter_dic = [key for key, value in dic.items() if max(dic.values()) == value]
+# 정렬 오름차순으로
+filter_dic.sort()
+if len(filter_dic) >= 2:
+	print(filter_dic[0])
+else:
+	print(filter_dic[0])
+	
---
+++ implementation(silver)/패션왕 신해빈(other solve).py
@@ -0,0 +1,34 @@
+# 문제분석
+# 뭔가 어딘가에 막혀서 잘 아노딘다..
+
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for _ in range(T):
+    clothes = {}
+    
+    n = int(input())
+    for _ in range(n):
+        v, k = input().split()
+        # 의류 종류가 dict에 없다면 새로 넣어주고, 아니면 원래 있던 의류 종류 키에 대해,
+        # set 밸류에 v 추가해주기
+        # 아 get 함수로 의상종류가 key값이기 때문에
+        # 의상 종류가 없다면 None을 반환하는구나
+        # 하지만 이것도 key 를 찾는거라 in 메서드랑 worst case 시간복잡도는 똑같다
+        # 하지만 이문제는 dic의 범위가 작기 때문에 in 메서드를 사용해도 큰 문제가 없다
+        if clothes.get(k) == None:
+            clothes[k] = set()
+        clothes[k].add(v)
+       
+    count = 1
+    for key in clothes.keys():
+    	count *= len(clothes[key]) + 1
+    print(count-1
+
+
+
+# 이 문제는 특정 옷을 분류하는데 까지는 성공했는데
+# 그 다음 필터링 처리하는 방법을 생각하지 못했다
+# 이 코드를 보니 공식화 시켜둔거 같은데
+# 이 공식이 어떻게 나온거지 ..
